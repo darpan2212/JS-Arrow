f71af3e662c463bf55f2e7c72a81b5b8545dbf33
Darpan <darpan.madlani@gmail.com>
2022-09-24 20:29:17 +0530

    Employee payroll object, try catch, getter setter

--- emp_payroll.js
+++ emp_payroll.js
@@ -1,26 +1,100 @@
 class EmployeePayroll {
 
     //properties
-    id;
-    salary;
 
-    constructor(id, name, salary) {
-        this.id = id;
-        this.salary = salary;
-        this.name = name;
+    constructor(...param) {
+        this.id = param[0];
+        this.name = param[1];
+        this.salary = param[2];
+        this.gender = param[3];
+        this.startDate = param[4];
     }
 
     get name() { return this._name; }
 
     set name(name) {
-        this._name = name;
+        let nameRegex = RegExp('^[A-Z]{1}[A-Za-z]{2,9}$');
+        if (nameRegex.test(name)) {
+            this._name = name;
+        } else {
+            throw 'Name is incorrect';
+        }
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    set id(id) {
+        if (id > 0) {
+            this._id = id;
+        } else {
+            throw 'ID must be non zero positive value';
+        }
+    }
+
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(salary) {
+        if (salary > 0) {
+            this._salary = salary;
+        } else {
+            throw 'salary must be non zero positive value';
+        }
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    set gender(gender) {
+        if (gender == 'M' || gender == 'F' || gender == 'O') {
+            this._gender = gender;
+        } else {
+            throw 'Gender must only from (M, F, O)';
+        }
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+
+    set startDate(startDate) {
+
+        let currentDate = new Date();
+        if (currentDate.getFullYear() >= startDate.getFullYear() && currentDate.getMonth() >= startDate.getMonth() && currentDate.getDate() > startDate.getDate()) {
+            this._startDate = startDate;
+        } else {
+            throw 'Date must not be future date'
+        }
     }
 
     toString() {
-        return "id : " + this.id + ", name : " + this._name + ", salary : " + this.salary;
+        return "id : " + this._id + ", name : " + this._name + ", salary : " + this._salary + ", gender : " + this._gender + ", startDate : " + this._startDate.toLocaleDateString();
     }
 }
+let startDate = new Date('2020-01-01');
 
-let emp = new EmployeePayroll(1, "Darpan", 1000000);
+let emp1 = new EmployeePayroll(1, "Darpan", 1000000, "M", startDate);
 
-console.log(emp.toString());
+console.log(emp1.toString());
+
+try {
+    let emp2 = new EmployeePayroll(2, "jack", 900100, "M", startDate);
+    console.log(emp2.toString());
+} catch (e) {
+    console.error(e);
+}
+
+console.log("Next implementations");
+startDate = new Date('2023-01-01');
+try {
+    let emp3 = new EmployeePayroll(3, "Mike", 1000000, "M", startDate);
+    console.log(emp3.toString());
+} catch (e) {
+    console.error(e);
+}
+
+console.log("Next implementations");
